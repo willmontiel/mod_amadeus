1915aae42fbad876020d0cfc854b37da7d68e706
Will Montiel <wmontiel@vectorial.co>
2017-11-29 17:46:25 -0500

    Data base

--- sql/mysql/install.mysql.utf8.sql
+++ sql/mysql/install.mysql.utf8.sql
@@ -1,11 +1,14 @@
 CREATE TABLE IF NOT EXISTS `#__amadeus` (
 	`id` int(10) NOT NULL AUTO_INCREMENT,
-	`hello` text NOT NULL,
-	`lang` varchar(25) NOT NULL,
+	`iata_code` varchar(10) NOT NULL,
+	`city_code` varchar(10) NOT NULL,
+    `city_name` varchar(80) NOT NULL,
+    `country_code` varchar(10) NOT NULL,
+    `country_name` varchar(80) NOT NULL,
+    `nbr_of_airport_codes` varchar(10) NOT NULL,
+    `airport_code` varchar(10) NOT NULL,
 
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-INSERT INTO `#__helloworld` (`hello`, `lang`) VALUES ('Hello World', 'en-GB');
-INSERT INTO `#__helloworld` (`hello`, `lang`) VALUES ('Hola Mundo', 'es-ES');
-INSERT INTO `#__helloworld` (`hello`, `lang`) VALUES ('Bonjour tout le monde', 'fr-FR');
+INSERT INTO `#__amadeus` (`iata_code`, `city_code`, `city_name`, `country_code`, `country_name`, `nbr_of_airport_codes`, `airport_code`) VALUES ('AAA', 'AAA', 'ANAA', 'PF', 'French Polynesia', '1', 'AAA');
