feee2bc38c0bc53d731b8e369c8cb8dd58ee535c
Will Montiel <wmontiel@vectorial.co>
2017-11-29 11:14:52 -0500

    Ajustes varios

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -1,7 +1,50 @@
 (function () {
   angular.module('amadeus.controllers', [])
-          .controller('ctrlSearchAmadeus', ['$scope', '$window', '$http', function ($scope, $window,$http) {
-              var self = this;
-              $scope.loading = false;
-            }]);
+        .controller('ctrlSearchAmadeus', ['$scope', '$window', '$http', function ($scope, $window,$http) {
+            var self = this;
+            $scope.loading = false;
+            $scope.openFilters = false;
+
+            self.isFromDisabled = false;
+            self.isFromCached = false;
+            self.selectedFrom = null;
+            self.cities = [
+                {
+                    name: 'Cali, Colombia (CLO)',
+                    description: 'Alfonso B. Aragon'
+                },
+                {
+                    name: 'Bogotá, Colombia (BOG)',
+                    description: 'El Dorado international'
+                },
+                {
+                    name: 'Barranquilla, Colombia (BAQ)',
+                    description: 'E. Cortissoz'
+                }
+            ];
+            self.querySearchFrom = querySearch;
+            self.selectedFromChange = selectedFromChange;
+
+            function querySearch (query) {
+                var results = query ? self.cities.filter( createFilterFor(query) ) : self.cities;
+                return results;
+            }
+
+            function createFilterFor(query) {
+                var lowercaseQuery = angular.lowercase(query);
+
+                return function filterFn(city) {
+                    res = angular.lowercase(city.name);
+                    return res.indexOf(lowercaseQuery.toLowerCase())!=-1;
+                };
+            }
+
+            function selectedFromChange(item) {
+                if (item) {
+                    self.selectedFrom = item;
+                } else {
+                    self.selectedFrom = null;
+                }
+            }
+        }]);
 })();
--- tmpl/default.php
+++ tmpl/default.php
@@ -14,43 +14,63 @@ $doc->addScript(JURI::base(true).'/modules/mod_amadeus/lib/angular-material/angu
 $doc->addScript(JURI::base(true).'/modules/mod_amadeus/js/mod_amadeus.js', 'text/javascript');
 $doc->addStyleSheet(JURI::base(true).'/modules/mod_amadeus/css/mod_amadeus.css');
 
-$doc->addScript(JURI::base(true).'/modules/mod_amadeus/js/app/app.js', 'text/javascript');
+$doc->addScript(JURI::base(true).'/modules/mod_amadeus/js/app/services.js', 'text/javascript');
 $doc->addScript(JURI::base(true).'/modules/mod_amadeus/js/app/controllers.js', 'text/javascript');
 $doc->addScript(JURI::base(true).'/modules/mod_amadeus/js/app/directives.js', 'text/javascript');
-$doc->addScript(JURI::base(true).'/modules/mod_amadeus/js/app/services.js', 'text/javascript');
+$doc->addScript(JURI::base(true).'/modules/mod_amadeus/js/app/app.js', 'text/javascript');
  
 ?>
  
-<div class="container-fluid" ng-app="amadeus" ng-controller="ctrlSearchAmadeus as Ctrl" ng-cloa> 
-    <div class="row">
-        <div class="col-md-6">
-            <div class="form-group">
+<div ng-app="amadeus" ng-controller="ctrlSearchAmadeus as ctrl" ng-cloak> 
+    <div layout="row" layout-align="space-between center">
+        <div>
+            <md-input-container class="all-width">
                 <label for="from">Desde</label>
-                <input type="text" class="form-control" id="from" name="from" placeholder="Aeropuerto o ciudad">
-                <small id="from" class="form-text text-muted">Escribe y selecciona el aeropuerto o ciudad de origen</small>
-            </div>
+                <md-autocomplete
+                    ng-disabled="ctrl.isFromDisabled"
+                    md-no-cache="ctrl.isFromCached"
+                    md-selected-item="ctrl.selectedFrom"
+                    md-search-text="ctrl.searchFromText"
+                    md-selected-item-change="ctrl.selectedFromChange(item)"
+                    md-items="item in ctrl.querySearchFrom(ctrl.searchFromText)"
+                    md-item-text="item.name"
+                    md-min-length="0"
+                    placeholder="Aeropuerto o ciudad">
+                    <md-item-template>
+                        <span md-highlight-text="ctrl.searchFromText" md-highlight-flags="^i">{{item.name}}</span>
+                    </md-item-template>
+                    <md-not-found>
+                        Se encontraron coincidencias con el texto "{{ctrl.searchFromText}}"
+                    </md-not-found>
+                </md-autocomplete>
+
+                <div class="h7 grey-fg">
+                    Escribe y selecciona el aeropuerto o ciudad de origen
+                </div>
+
+            </md-input-container>
         </div>
         
-        <div class="col-md-6">
+        <div>
             <div class="form-group">
                 <label for="from">Hasta</label>
                 <input type="text" class="form-control" id="to" name="to" placeholder="Aeropuerto o ciudad">
-                <small id="to" class="form-text text-muted">Escribe y selecciona el aeropuerto o ciudad de destino</small>
+                <div class="h7 grey-fg">
+                    Escribe y selecciona el aeropuerto o ciudad de destino
+                </div>
             </div>
         </div>
     </div>
 
-    <div class="row mt-5">
-        <div class="col-md-6">
+    <div ng-if="openFilters" class="mt-5">
+        <div layout="row" layout-align="start center">
             <div class="form-check">
                 <label class="form-check-label">
                     <input class="form-check-input" type="radio" name="route" id="route1" value="1" checked>
                     Ida y vuelta
                 </label>
             </div>
-        </div>
-        
-        <div class="col-md-6">
+
             <div class="form-check">
                 <label class="form-check-label">
                     <input class="form-check-input" type="radio" name="route" id="route2" value="2">
@@ -58,40 +78,35 @@ $doc->addScript(JURI::base(true).'/modules/mod_amadeus/js/app/services.js', 'tex
                 </label>
             </div>
         </div>
-    </div>
 
-    <div class="row mt-5">
-        <div class="col-md-6">
+        <div layout="row" layout-align="space-between center">
             <div class="form-group">
                 <label for="from">Fecha de salida</label>
                 <input type="text" class="form-control" id="from" name="from" placeholder="Aeropuerto o ciudad">
                 <small id="from" class="form-text text-muted">Escribe y selecciona el aeropuerto o ciudad de origen</small>
             </div>
-        </div>
-        
-        <div class="col-md-6">
+
             <div class="form-group">
                 <label for="from">Fecha de regreso</label>
                 <input type="text" class="form-control" id="from" name="from" placeholder="Aeropuerto o ciudad">
                 <small id="from" class="form-text text-muted">Escribe y selecciona el aeropuerto o ciudad de origen</small>
             </div>
         </div>
-    </div>
 
-    <div class="row mt-5">
-        <div class="col-md-6">
+        <div layout="row" layout-align="space-between center">
             <div class="form-group">
-                <label for="from">Fecha de salida</label>
+                <label for="from">Adultos</label>
                 <input type="text" class="form-control" id="from" name="from" placeholder="Aeropuerto o ciudad">
-                <small id="from" class="form-text text-muted">Escribe y selecciona el aeropuerto o ciudad de origen</small>
             </div>
-        </div>
-        
-        <div class="col-md-6">
+
             <div class="form-group">
-                <label for="from">Fecha de regreso</label>
+                <label for="from">Niños</label>
                 <input type="text" class="form-control" id="from" name="from" placeholder="Aeropuerto o ciudad">
-                <small id="from" class="form-text text-muted">Escribe y selecciona el aeropuerto o ciudad de origen</small>
+            </div>
+
+            <div class="form-group">
+                <label for="from">Infantes</label>
+                <input type="text" class="form-control" id="from" name="from" placeholder="Aeropuerto o ciudad">
             </div>
         </div>
     </div>
